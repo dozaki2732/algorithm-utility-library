eab6119980dd15b1abc0e86e6e01b30f541b3aa2
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-05 11:08:59 -0700

    functions

--- functions.js
+++ functions.js
@@ -3,3 +3,63 @@ function convertCtoF(celsius) {
    let fahrenheit = celsius * (9 / 5) + 32;
    return fahrenheit;
 }
+
+// Reverse a String
+function reverseString(str) {
+   let result = str.split("").reverse().join("");
+   return result;
+}
+
+// Factorialize a Number
+function factorializeNumber(num) {
+   let result = 1;
+   for (let i = num; i > 0; i--) {
+      result *= i;
+   }
+   return result;
+}
+
+// Find the Longest Word in a String
+function findLongestWordLength(str) {
+   let splitStr = str.split(" ");
+   let mapStr = splitStr.map((word) => word.length);
+   let reducedStr = mapStr.reduce((longestLength, wordLength) => {
+      if (wordLength > longestLength) {
+         return wordLength;
+      } else {
+         return longestLength;
+      }
+   }, 0);
+   console.log(reducedStr);
+   return reducedStr;
+}
+
+// Return Largest Numbers in Arrays
+
+function largestOfFour(arr) {
+   var array = [];
+   for (let i in arr) {
+      var firstReduce = arr[i].reduce((largestNum, num) => {
+         if (largestNum < num || largestNum === null) {
+            return num;
+         } else {
+            return largestNum;
+         }
+      }, null);
+      console.log(firstReduce);
+      array.push(firstReduce);
+   }
+   return array;
+}
+
+// Confirm the Ending
+// Repeat a String Repeat a String
+// Truncate a String
+// Finders Keepers
+// Boo who
+// Title Case a Sentence
+// Slice and Splice
+// Falsy Bouncer
+// Where do I Belong
+// Mutations
+// Chunky Monkey
